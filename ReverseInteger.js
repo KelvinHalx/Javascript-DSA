a2a429c4bf3f6ccf66d0239c54b8dc7f0ebbf399
KelvinHalx <kelvinwainaina03@gmail.com>
2022-08-23 16:10:29 +0300

    Reverse Integer

---
+++ ReverseInteger.js
@@ -0,0 +1,16 @@
+// Algorithm to reverse an integer
+I
+// 1. convert to a string
+// 2. turn into array
+// 3. reverse method
+// 4. back to string
+// 5. back to int
+
+const reverseInt = (n)=> {
+    return parseInt(n.toString().split('').reverse().join(''))
+} 
+
+console.log({
+    
+})
+
