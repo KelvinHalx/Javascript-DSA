d618017738584fccdf0cbcd8c54a1a452d2c9e85
KelvinHalx <kelvinwainaina03@gmail.com>
2022-08-25 12:28:52 +0300

    Update stringReverse.js

--- stringReverse.js
+++ stringReverse.js
@@ -15,3 +15,14 @@ stringReverse('sparepap');
 
 // Expected Outcome
 // paperaps
+
+// Solution 2
+function reverse1(str){
+  let r = "";
+  for(let i = str.length-1; i >= 0; i--){
+    r += str[i];
+  }
+  return r;
+}
+
+console.log(reverse1("stepsolution"))
